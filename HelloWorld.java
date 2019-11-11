43729d4eb32a72dce5e7959e1d32dbcbe13e806c
Mohit Jangra <mohitjangra07@gmail.com>
2019-11-11 22:01:23 +0530

    Added Good Afternoon message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("Good Morning")
+		System.out.println("Good Morning");
+		System.out.println("Good Afternoon");
 	}
 }
